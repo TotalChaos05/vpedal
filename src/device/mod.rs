559b2be24982ec5a8d45e4c208bab9d042c50122
Basil Keeler <70387628+TotalChaos05@users.noreply.github.com>
2023-09-12 20:33:50 -0500

    create device trait

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -1,3 +1,8 @@
-pub struct Device {
-  
+use ringbuf::HeapRb;
+
+pub trait Device {
+  fn input_stream() -> HeapRb::<i16>;
+  fn output_stream() -> HeapRb::<i16>;
+  fn next_input() -> i16;
+  fn next_output() -> i16;
 }
